91b75c395b752f65df4d58a9d78b72a687a725fe
RontiOluwa <olaoluwaojedapo@gmail.com>
2024-03-26 19:03:25 +0100

    feat: made first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
 const path = require('path')
 
 const webpackBase = {
-  ...require('../../build/webpack/webpack.config.base')
+  // ...require('../../build/webpack/webpack.config.base')
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: ['node_modules'],
+  },
+
+  // plugins: [
+  //   // new webpack.EnvironmentPlugin({
+  //   //   NODE_ENV: 'production',
+  //   // }),
+
+  //   new webpack.NamedModulesPlugin(),
+  // ],
 }
 
 const main = {
